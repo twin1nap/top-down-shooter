11a71daa5c2f980bcf6f202c277f9f7886ba7d53
JillevBeers <410006926@st.roc.a12.nl>
2025-03-12 14:34:40 +0100

    starting sceen finished

---
+++ top down shooter/Assets/StartingScreenScipt.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class StartingScreenScipt : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+    public Button ButtonStart;
+    public Button ButtonQuit;
+
+    // Update is called once per frame
+    void Update()
+    {
+       
+
+    }
+
+
+    public void startGameBTN()
+    {
+        // Start level 1
+        Debug.Log("STARTING WORKS");
+        //SceneManager.LoadScene("Level_1");  
+    }
+    public void QuitBTN()
+    {
+        // Stopt de game
+        Debug.Log("QUITING WORKS");
+        //Application.Quit();
+    }
+}
