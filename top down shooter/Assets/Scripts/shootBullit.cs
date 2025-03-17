daefcecf4e0a07c896d99906f4051acdcd71e0f5
Joran Nap <143212549+twin1nap@users.noreply.github.com>
2025-03-17 11:53:29 +0100

    fixed shooting

--- top down shooter/Assets/Scripts/shootBullit.cs
+++ top down shooter/Assets/Scripts/shootBullit.cs
@@ -9,6 +9,9 @@ public class shootBullit : MonoBehaviour
     public GameObject bullit;
     public Transform shootPoint;
     public float speed = 1000f;
+
+    public float fireRate = 0.1f; // Adjust for desired RPM (0.1s = 600 RPM, 0.066s = 900 RPM, 0.08s = 750 RPM)
+    private float timer;
     // Start is called before the first frame update
     void Start()
     {
@@ -18,9 +21,12 @@ public class shootBullit : MonoBehaviour
     // Update is called once per frame
     private void Update()
     {
+        timer += Time.deltaTime;
         
-        if (Input.GetMouseButtonDown(0))
+        if (Input.GetMouseButton(0) && timer >= fireRate)
         {
+            timer = 0;
+
             var tempball = Instantiate(bullit, shootPoint.position, shootPoint.rotation);
             tempball.GetComponent<Rigidbody2D>().AddForce(shootPoint.right * speed);
             Destroy(tempball, 10f);
