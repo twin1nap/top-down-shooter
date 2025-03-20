d19c629055df077f78d1bf212e4e614cb3ea4a8f
JillevBeers <410006926@st.roc.a12.nl>
2025-03-20 09:42:38 +0100

    added some statements to the shooting  scipt
    i added this cuz now he fills the mag whit the last 30 bullets, then he makes the mag 0, so it wont go negative... basicly our game is kinda done,
    
    if your reading this.. should we make this an release for an full finished version?
    
    ciao

--- top down shooter/Assets/Scripts/shootBullit.cs
+++ top down shooter/Assets/Scripts/shootBullit.cs
@@ -12,6 +12,7 @@ public class shootBullit : MonoBehaviour
     public GameObject bullit_prefab;
     public Transform shootPoint;
     public float speed = 1000f;
+    public TextMeshProUGUI ammoTEXT; 
 
     public float fireRate = 0.1f; // Adjust for desired RPM (0.1s = 600 RPM, 0.066s = 900 RPM, 0.08s = 750 RPM)
     private float timer;
@@ -41,31 +42,37 @@ public class shootBullit : MonoBehaviour
 
         }
 
-        else if (Input.GetMouseButton(0) && Mag <= 0 && Ammo >= 0)
+        else if (Input.GetMouseButton(0) && Mag <= 0 && Ammo > 0) // makes sure there is amo, then reload
         {
-            if (!reloading)
+            if (!reloading) // checks if reloading is being done
             {
-            Debug.Log("test");
-                reloading = true;
-                timer_reload = 0;
+                Debug.Log("test");
+                reloading = true; // Start the reaload function
+                timer_reload = 0; // Sets the reaload timer on zero
             }
-            
         }
-        if (reloading)
+
+        if (reloading) 
         {
             timer_reload += Time.deltaTime;
             Debug.Log(timer_reload);
-            if (timer_reload >= 1.4)
+
+            if (timer_reload >= 1.4f) // if reaload time is finished
             {
                 Debug.Log("reloaded");
-                Mag += 30;
-                Ammo -= 30;
 
-                if (Ammo == 0)
+                if (Ammo >= 30) // if there is enough ammo or more then 30
                 {
-                    gamehandeler.ammo_text.text = "0/0";
-                } 
-                reloading = false;
+                    Mag += 30; // adds the mag whit 30
+                    Ammo -= 30; // removes 30 out of the inventory
+                }
+                else // when there less then 30 ammo
+                {
+                    Mag += Ammo; //fills the mag whit the leftover
+                    Ammo = 0; // ammo on zero, so it would not go negative
+                }
+
+                reloading = false; // stops realoading
             }
         }
     }
